9fe952045fd63e2b74045de4c3c1a41e51b7292b
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-24 00:33:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,3 +8,14 @@ counter += 1
 end 
 return new_array
 end 
+
+
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
