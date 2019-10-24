47d46c7cdb8719444e8ea8eee84197354b01f707
Tony Rim <duc_thinh_03@yahoo.com>
2019-10-24 00:38:56 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
-end
+def square_array(numbers)
+  i = 0
+  new_array = []
+  
+while i < numbers.size 
+new_array.push(numbers[i] * numbers[i])
+i += 1
+end 
+return new_array
+end 
+
